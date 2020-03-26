e7ce316695dd51236ec1a3f55dbb496c446fa057
thibaut54 <john_milton100@hotmail.com>
2020-03-26 01:30:12 +0100

    config add config and security: username/pwd are now asked to access dashboard and to register

--- src/main/java/org/thibaut/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/org/thibaut/discoveryservice/DiscoveryServiceApplication.java
@@ -3,6 +3,14 @@ package org.thibaut.discoveryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.stereotype.Component;
+
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -12,4 +20,18 @@ public class DiscoveryServiceApplication {
 		SpringApplication.run( DiscoveryServiceApplication.class, args );
 	}
 
+//	@Bean
+//	public SecurityWebFilterChain springSecurityFilterChain( ServerHttpSecurity http,
+//	                                                         ReactiveClientRegistrationRepository clientRegistrationRepository) {
+//
+//		// Require authentication for all requests
+//		http.cors().and().authorizeExchange().anyExchange().permitAll();
+//
+//		// Allow showing /home within a frame
+////		http.headers().frameOptions().mode( XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN);
+//
+//		// Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
+//		http.csrf().disable();
+//		return http.build();
+//	}
 }
---
+++ src/main/java/org/thibaut/discoveryservice/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package org.thibaut.discoveryservice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@EnableWebSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//	@Override
+//	public void configure( AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication()
+//				.passwordEncoder( NoOpPasswordEncoder.getInstance())
+//				.withUser("admin").password("admin")
+//				.authorities("ADMIN");
+//	}
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//				.csrf().disable().authorizeRequests().anyRequest().authenticated()
+//				.and().httpBasic();
+//	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable() //
+				.authorizeRequests() //
+				.antMatchers(HttpMethod.GET, "/eureka/**").authenticated() // eureka client
+				.antMatchers(HttpMethod.POST, "/eureka/**").authenticated() // eureka client
+				.antMatchers(HttpMethod.DELETE, "/eureka/**").authenticated() // eureka client
+				.anyRequest().authenticated().and().httpBasic(); // dashboard authorization
+	}
+
+//    @Override
+//    protected void configure( HttpSecurity http) throws Exception {
+//        http.csrf().ignoringAntMatchers("/eureka/**").disable();
+//        super.configure(http);
+//    }
+
+//	@Override
+//	protected void configure( HttpSecurity http ) throws Exception {
+//		http.sessionManagement( )
+//				.sessionCreationPolicy( SessionCreationPolicy.ALWAYS )
+////				.and( ).requestMatchers( ).antMatchers( "/eureka/**" )
+//				.and( ).authorizeRequests( ).mvcMatchers( "/eureka/**" ).hasAnyRole( "ADMIN", "admin" )
+////				.anyRequest( ).denyAll( )
+//				.and( ).httpBasic( ).and( ).csrf( ).disable( );
+//	}
+}
